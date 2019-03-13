ee0bae2ae1e9334586729724c9291cc04e7270cf
Kenny <jerryice0017@gmail.com>
2019-03-13 14:50:58 +0800

    commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/homepage', function (req, res) {
+    res.render('homepage');
+  });
+
+var port = 4000
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
