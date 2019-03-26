f7717521baa50582d78dc2cebf17d88213619a58
Kenny <jerryice0017@gmail.com>
2019-03-26 16:04:02 +0800

    update version

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render('homepage');
-  });
+app.get('/', function(req, res) {
+	res.render('homepage');
+});
 
-var port = 4000
-app.listen(port, function () {
-    console.log(`Example app listening on port ${port}!`);
+var port = 4000;
+app.listen(port, function() {
+	console.log(`Example app listening on port ${port}!`);
 });
